14e05a9765e98fc18bb7ffa7b04038438fa9f800
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2021-11-16 12:32:16 +0200

    Initial Commit

---
+++ PR7.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PR7.1
+//
+//  Created by mac on 11/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
