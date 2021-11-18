b2c750db844cd57b0fcde2b4655820bf803c46d7
ishtvankomka <ishtvan.komonii.it.2021@lpnu.ua>
2021-11-18 02:41:22 +0200

    first master commit

--- PR7.1/main.cpp
+++ PR7.1/main.cpp
@@ -1,14 +1,86 @@
-//
-//  main.cpp
-//  PR7.1
-//
-//  Created by mac on 11/16/21.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int** a, const int rowCount, const int colCount, const int Low,
+const int High); void Change(int** a, const int row1, const int row2, const int colCount);
+void Print(int** a, const int rowCount, const int colCount); void Sort(int** a, const int rowCount, const int colCount);
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k);
+int main()
+{
+srand((unsigned)time(NULL)); int Low = 7;
+    int High = 62;
+    int rowCount = 7;
+    int colCount = 6;
+    int **a = new int*[rowCount];
+     for (int i=0; i<rowCount; i++)
+         a[i] = new int[colCount];
+    Create(a, rowCount, colCount, Low, High);
+    Print(a, rowCount, colCount);
+    Sort(a, rowCount, colCount);
+    Print(a, rowCount, colCount);
+    int S=0;
+    int k=0;
+    Calc(a, rowCount, colCount, S, k);
+    cout << "S = " << S << endl;
+    cout << "k = " << k << endl;
+    Print(a, rowCount, colCount);
+    for (int i=0; i<rowCount; i++)
+        delete [] a[i];
+    delete [] a;
     return 0;
+}
+void Create(int** a, const int rowCount, const int colCount, const int Low, const int High)
+{
+    for (int i=0; i<rowCount; i++)
+    {
+         for (int j=0; j<colCount; j++)
+         {
+              a[i][j] = Low + rand() % (High-Low+1);
+         }
+    }
+}
+void Print(int** a, const int rowCount, const int colCount) {
+    cout << endl;
+    for (int i=0; i<rowCount; i++)
+    {
+        for (int j=0; j<colCount; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+void Sort(int** a, const int rowCount, const int colCount)
+{
+for (int i0=0; i0<colCount-1; i0++) for (int i1=0; i1<colCount-i0-1; i1++)
+if (( a[0][i1] < a[0][i1+1] ) ||
+( a[0][i1] == a[0][i1+1] &&
+a[1][i1] < a[1][i1+1] ) ||
+( a[0][i1] == a[0][i1+1] &&
+  a[1][i1] > a[1][i1+1] &&
+  a[2][i1] >  a[2][i1+1] ))
+Change(a, i1, i1+1, rowCount);
+}
+void Change(int** a, const int col1, const int col2, const int rowCount)
+{
+    int tmp;
+    for (int i=0; i<rowCount; i++)
+    {
+        tmp = a[i][col1];
+        a[i][col1] = a[i][col2];
+        a[i][col2] = tmp;
+    }
+}
+void Calc(int** a, const int rowCount, const int colCount, int& S, int& k)
+{
+    S = 0;
+    k = 0;
+    for (int i=0; i<rowCount; i++)
+    for (int j=0; j<colCount; j++)
+    if (a[i][j] % 2 == 0 && !((i + j) % 8 == 0))
+    {
+        S += a[i][j];
+        k++;
+        a[i][j] = 0;
+    }
 }
